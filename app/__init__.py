82809aa17a0b887217238e0e365150f3853224d3
Sangwoo <newksw121@gmail.com>
2014-12-05 02:28:39 +0000

    Add manage.py for db migration
    - requirements.txt includes necessary flask extentions

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 
+import os
+
 app = Flask(__name__)
 
-app.config.from_object('config.DevelopmentConfig')
+app.config.from_object(os.environ['APP_SETTINGS'])
 db = SQLAlchemy(app)
